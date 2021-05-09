24551847d54c98925c7cdf78fb062cfcbd9212ec
Krzysztof <Krzysztof@DESKTOP-BNSLHP3>
2021-05-09 11:29:40 +0200

    add collision interpreting

--- ObjectLogic/CollisionInterpreter.cpp
+++ ObjectLogic/CollisionInterpreter.cpp
@@ -1,2 +1,19 @@
 #include "pch.h"
 #include "CollisionInterpreter.h"
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+
+void Logic::CollisionInterpreter::InterpretCollision(std::shared_ptr<ILiveObject>& object1, std::shared_ptr<ILiveObject>& object2)
+{
+		auto liveParams1 = object1->GetLiveParameters();
+		auto liveParams2 = object2->GetLiveParameters();
+		std::visit(*this, liveParams1, liveParams2);
+		object1->SetLiveParameters(liveParams1);
+		object2->SetLiveParameters(liveParams2);
+		
+}
--- ObjectLogic/CollisionInterpreter.h
+++ ObjectLogic/CollisionInterpreter.h
@@ -1,28 +1,40 @@
 #pragma once
+
+#include "ILiveObject.h"
 #include "ObjectLogic.h"
 #include "Bullet.h"
 
-class CollisionInterpreter
+
+namespace Logic
+{
+
+
+class EXPORT_OBJECT_LOGIC CollisionInterpreter
 {
 public:
+	void InterpretCollision(std::shared_ptr<ILiveObject>& object1, std::shared_ptr<ILiveObject>& object2);
+//private:
+
 	template <typename Object1, typename Object2>
-	void InterpretCollision(Object1& object1, Object2& object2);
+	void operator ()(Object1& object1, Object2& object2);
 	
 };
 
 template<typename Object1, typename Object2>
-inline void CollisionInterpreter::InterpretCollision(Object1& object1, Object2& object2)
+inline void CollisionInterpreter::operator ()(Object1& object1, Object2& object2)
 {
 	std::cout << "Collision interpretation is not implemented" << std::endl;
 }
 template<>
-inline void CollisionInterpreter::InterpretCollision<Logic::ObjectLogic, Logic::Bullet>(Logic::ObjectLogic& object1, Logic::Bullet& bullet)
+inline void CollisionInterpreter::operator () <Logic::ObjectLogic, Logic::Bullet>(Logic::ObjectLogic& object1, Logic::Bullet& bullet)
 {
 	object1.currentLive-=bullet.Damage;
 	bullet.Used = true;
 }
 template<>
-inline void CollisionInterpreter::InterpretCollision<Logic::ObjectLogic, Logic::ObjectLogic>(Logic::ObjectLogic& object1, Logic::ObjectLogic& object2)
+inline void CollisionInterpreter::operator () <Logic::ObjectLogic, Logic::ObjectLogic>(Logic::ObjectLogic& object1, Logic::ObjectLogic& object2)
 {
 	object2.currentLive-= object1.damage;
+}
+
 }
