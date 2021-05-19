45c80bbacfb756555740ab1e4bb7e2a4e3cb80e3
Krzysztof <Krzysztof@DESKTOP-BNSLHP3>
2021-05-19 16:56:03 +0200

    delete unused projects

--- Gra/CharacterFactory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <memory>
-#include "Character.h"
-class EXPORT_CHARACTER CharacterFactory
-{
-public:
-	ICharacter* make_character(const std::string& name);
-};
-
--- Gra/IInteraction.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ICharacter.h"
-
-class IInteraction {
-public:
-	bool virtual action(std::unique_ptr<ICharacter> &ofenser, std::unique_ptr<ICharacter> &target) =0;
-
-};
