c8fea67ee33a6241d91b1ff531421a7e2d830f68
Krzysztof <krzysztof.majda@tricholab.com>
2021-07-10 23:23:50 +0200

    delete unused clases

--- OpenGL/FrenderFunctions.cpp
+++
@@ -1,6 +0,0 @@
-#include "FrenderFunctions.h"
-
-void Render::RenderObject(const std::shared_ptr<IObject>& object, Program& program)
-{
-	
-}
--- OpenGL/FrenderFunctions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Program.h"
-#include "..\Core\IObject.h"
-
-
-namespace Render
-{
-	void RenderObject(const std::shared_ptr<IObject>& object, Program& program);
-}
